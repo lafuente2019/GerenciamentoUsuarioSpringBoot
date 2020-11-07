876ef6617db5e5c2af0509b0b27860b2131d08c8
Valter Lafuente Junior <valterlafuente@yahoo.com.br>
2020-11-07 13:17:12 -0300

    Projeto criado

---
+++ src/main/java/br/senac/tads/projetospring/ProjetoSpringApplication.java
@@ -0,0 +1,13 @@
+package br.senac.tads.projetospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringApplication.class, args);
+	}
+
+}
